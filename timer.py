c7cecb762ca35c4751f23d2ca5341b1bee51bb30
GonetoPlaidUK <allanw74@gmail.com>
2022-01-04 17:06:39 +0000

    removed 3 satellites

--- timer.py
+++ timer.py
@@ -4,7 +4,4 @@ while True:
     sleep(1 - time() % 1)
     exec(open('hst1.py').read()) 
     exec(open('iss.py').read())
-    exec(open('cosmos1408.py').read())
-    exec(open('cosmos1536.py').read())
-    exec(open('genesis1.py').read())
     exec(open('TIANHE.py').read())
