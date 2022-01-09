0bb08d5634389f2d6d5ea4003d6832e854061ffe
GonetoPlaidUK <allanw74@gmail.com>
2022-01-09 10:13:49 +0000

    starlink added

---
+++ starlink.py
@@ -0,0 +1,74 @@
+from skyfield.api import load, wgs84, load_file
+
+import json
+import os
+stations_url = 'http://celestrak.com/NORAD/elements/starlink.txt'
+satellites = load.tle_file(stations_url)
+
+
+
+by_name = {sat.name: sat for sat in satellites}
+
+f = open( './html/starlinkapi.txt', 'w' )
+f.write( '[')
+f.close()
+
+for i in by_name:
+    number = str(i)
+    satellite = by_name[number]
+
+
+    ts = load.timescale()
+
+
+
+    t = ts.now()
+
+
+    geocentric = satellite.at(t)
+    lat, lon = wgs84.latlon_of(geocentric)
+    height = wgs84.height_of(geocentric)
+             
+    latitude = str(lat)
+    longitude = str(lon)
+
+    alt = str(height)                
+    z = alt.split('e')     
+    zz = z[0]
+    alti = float(zz) * 149.597871
+    altitude = round(alti,2)     
+
+
+    x = latitude.split('deg')
+    xx = ''.join(x)
+    xxx = xx.split('\'')
+    xxxx = ''.join(xxx)
+    xxxxx  = xxxx.split('\"')
+    xxxxxx = ''.join(xxxxx)
+    xxxxxxx= xxxxxx.split()
+    xxxxxxxx = ''.join(xxxxxxx)
+    deglat = float(xxxxxxxx)/10000
+
+
+    yy = longitude.split('deg')
+    yyy = ''.join(yy)
+    yya = yyy.split('\'')
+    yyb = ''.join(yya)
+    yyc  = yyb.split('\"')
+    yyd = ''.join(yyc)
+    yye= yyd.split()
+    yyf = ''.join(yye)
+    deglon = float(yyf)/10000
+                
+
+
+    f = open( './html/starlinkapi.txt', 'a' )
+    f.write( '{"latitude":' + str(deglat) + ',"longitude":' + str(deglon) + ',"altitude":' + str(altitude) + '},')
+    f.close()
+
+with open('./html/starlinkapi.txt', 'rb+') as filehandle:
+    filehandle.seek(-1, os.SEEK_END)
+    filehandle.truncate()
+f = open( './html/starlinkapi.txt', 'a' )
+f.write(']')
+f.close()
